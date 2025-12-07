1e7a47c16d1020637aa0fd4ec3c8b0c61f1e32e6
Victor Hugo Bitu <vhbitu@gmail.com>
2025-12-07 10:13:46 -0300

    feat(terraform): adiciona variáveis genéricas para multiambientes

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "ID do projeto GCP"
+  type = string
+}
+
+variable "region" {
+  description = "Região padrão do GCP"
+  type = string
+  default = "southamerica-east1"
+}
+
+variable "env" {
+  description = "Nome do ambiente (dev ou prod)"
+  type = string
+  default = "dev"
+}
